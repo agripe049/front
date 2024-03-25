0c23776541bb7154c061f2db45dbf71e388375c2
agripe049 <agripeheber@gmail.com>
2024-03-25 20:40:08 -0300

    teste

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Form from './components/Form'
 import List from './components/List'
 import { Task } from './Task'
+import API from './utils/API'
 
 function App(): JSX.Element {
   const [tarefas, setTarefas] = useState<Task[]>([])
+  
+  const getTasks = async () => {
+    const {data , error} = await API.get('/tasks')
+    if(data.length > 0){
+      setTarefas(data)
+    }
+    
+  }
+
+  useEffect(()=>{
+    getTasks()
+
+  },[])
+  
   return (
     <>
       <Form setTarefas={setTarefas} tarefas={tarefas} />
